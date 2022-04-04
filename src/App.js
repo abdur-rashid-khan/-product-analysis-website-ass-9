8aee9bfe8c016b9b9fc7756b68d1266a1e620fc5
Rashid khan <rashidkhanbd57@gmail.com>
2022-04-04 16:10:34 +0600

    ans on number qusen

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/reviews' element={<Review></Review>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
-        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/blogs' element={<Blog></Blog>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='*' element={<NoDataFound></NoDataFound>}></Route>
       </Routes>
--- src/Components/Blogs/Blog.js
+++ src/Components/Blogs/Blog.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Blog = () => {
    return (
-      <div>
+      <div className='sm:container md:container lg:container xl:container mx-auto px-4 pt-20'>
          <div>
             <p>1. what is contextApi?</p>
             <strong> Ans : Prop drilling also called threading refers to the process you have to go through to get data to parts of the React Component tree</strong>
+         </div>
+         <div>
+            
          </div>
       </div>
    );
